667b7959600970f1dbd67f5ba87336c9b3a80a37
Noyan Gunday <ngunday@gmail.com>
2019-06-02 23:44:38 +0100

    Updates

---
+++ precache-manifest.91881c6afb82e969d1a683a79d2611b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707f7d855dc2d0335c6532da8d8048a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c6a0a0d55aa5d40bb4",
+    "url": "/static/css/main.7c6be864.chunk.css"
+  },
+  {
+    "revision": "8f61ee350e6a5ef5f4e0",
+    "url": "/static/js/2.21ac127f.chunk.js"
+  },
+  {
+    "revision": "18c6a0a0d55aa5d40bb4",
+    "url": "/static/js/main.6edd606d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
